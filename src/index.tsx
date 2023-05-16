b43e05d2ff0c64fc95cfce96a8606c54d2ae6e42
nsentemova <sentemovanatalya@yandex.ru>
2023-05-16 22:18:44 +0300

    extra fonts deleted

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./state/store";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/latin-400.css";
+import "@fontsource/roboto/latin-500.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
